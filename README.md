fa55e26bd7b594da4910aeebf1a83afd84a57c5b
edoardodraetta <edoardoluigi.draetta@studenti.unimi.it>
2021-03-29 15:21:49 +0200

    updated README

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ LSN exercise delivery.
 
 ### Exercise 1
 
-Run `make` in `./rng/` to generate `main.exe`. 
+Run `make` in `./rng/` to generate `main.exe`, which will run all experiments in exercise 1. 
 
+The jupyter notebook needs to be run sequentially. 
+
+#### TO DO:
+[ ] - Is the Cauchy fit a true fit?
+[ ] - blocked chi2 as histogram
+[ ] - Double check python code
--- esercizio_1/rng/main.cpp
+++ esercizio_1/rng/main.cpp
@@ -12,62 +12,25 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 #include <fstream>
 #include <string>
 #include <vector>
-#include "random.h"
 
+#include "random.h"
+#include "functions.h"
 using namespace std;
 
-float make_pi(int hits, int n_throws, float d, float length){
-   if (hits==0) return 0;
-   else {
-      return (2*length*n_throws) / (hits * d);
-   }
-}
-
-float error(vector<float> AV, vector<float> AV2, int n){
-   if (n == 0){
-      return 0;
-   } else {
-      return sqrt( (AV2[n] - AV[n]*AV[n]) / n);
-   }
-}
-
-void blocked_stats(vector<float>  AV, vector<float>  AV2, int N, string filename){
-   ofstream outfile;
-   outfile.open(filename);
-
-   vector<float> sum(N,0);
-   vector<float> sum2(N,0);
-   vector<float> err(N,0);
-
-   for (int i=0; i<N; i++){
-      for (int j=0; j<(i+1); j++){
-         sum[i] += AV[j]; // cumulative sum of averages
-         sum2[i] += AV2[j]; // sum of square averages
-      }
-      sum[i] /= (i+1); // cumulative average
-      sum2[i] /= (i+1); // cumulative square average
-      err[i] = error(sum, sum2, i); // uncertainty
-      outfile << sum[i] << " " << sum2[i] << " " << err[i] << endl;
-   }
-   outfile.close();
-}
 
 int main (int argc, char *argv[]){
 
+   // Random number generator
 
    Random rnd;
    int seed[4];
    int p1, p2;
-
-   // Read Primes and Seeds.in
 
    ifstream Primes("Primes");
    if (Primes.is_open()){
       Primes >> p1 >> p2 ;
    } else cerr << "PROBLEM: Unable to open Primes" << endl;
    Primes.close();
-
-   cout << "Primes: " << p1 << " " << p2 << endl;
 
    ifstream input("seed.in");
    string property;
@@ -82,59 +45,51 @@ int main (int argc, char *argv[]){
       input.close();
    } else cerr << "PROBLEM: Unable to open seed.in" << endl;
 
+   cout << "Primes: " << p1 << " " << p2 << endl;
    cout << "Seed: ";
    for (int i=0; i<4; i++){
       cout << seed[i] << " ";
    }
    cout << endl;
 
-   // 1.1.1
+   // 1.1.1 First Integral
 
    int M = 100000;
    int N = 100;
    int L = M/N;
-
-   vector<float> ave(N,0);
-   vector<float> av2(N,0);
-   vector<float> sum_prog(N,0);
-   vector<float> su2_prog(N,0);
-   vector<float> err_prog(N,0);
-
-   float sum, r;
-   string datafile;
+   vector<double> ave(N,0);
+   vector<double> av2(N,0);
 
    // Integrate
 
    for (int i = 0; i < N; i++) {
-      sum = 0;
       for (int j = 0; j < L; j++) {
-         sum += rnd.Rannyu();
+         ave[i] += rnd.Rannyu();
       }
-      ave[i] = sum/L;
+      ave[i] /= L;
       av2[i] = (ave[i]*ave[i]);
    }
 
    // Cumulative blocked statistics
 
-   datafile = "./data/stats_1.1.1.dat";
+   string datafile = "./data/stats_1.1.1.dat";
    blocked_stats(ave, av2, N, datafile);
 
-   // 1.1.2
+   // 1.1.2 Second Integral
 
    rnd.SetRandom(seed,p1,p2);
-
    fill(ave.begin(), av2.end(), 0);
    fill(av2.begin(), av2.end(), 0);
 
    // Integrate
 
+   double r;
    for (int i=0; i < N; i++) {
-      sum = 0;
       for (int j=0; j < L; j++) {
          r = rnd.Rannyu();
-         sum += (r-0.5) * (r-0.5);
+         ave[i] += (r-0.5) * (r-0.5);
       }
-      ave[i] = sum/L;
+      ave[i] /= L;
       av2[i] = (ave[i]*ave[i]);
    }
 
@@ -145,162 +100,113 @@ int main (int argc, char *argv[]){
 
    // 1.1.3 Chi Squared Test
 
-   // M = 1000000;
-   // L = M/N;
-   // float chi2_sum = 0;
-   // float chi2_prog[N] = {0};
-   // int n = L/N;
-   // int count;
+   M = 1000000; // throws
+   N = 100;     // blocks
+   L = M/N;     // throws per block
+   int n = L/N; // Expectation value for chi2 test
+   int count;   // Experimental value for chi2 test
+   float a,b;   // Lower and upper bounds
 
+   float chi2_sum = 0;
+   vector<double> chi2_prog(N,0);
 
-   // statsfile.open("./data/stats_1.1.3.dat");
+   ofstream statsfile;
+   statsfile.open("./data/stats_1.1.3.dat");
 
-   // for (int i=0; i<N; i++){
-   //    count = 0;
-   //    float r; // ok ?
-   //    for (int j=0; j<L; j++){
+   for (int i=0; i<N; i++){
+      count = 0;
+      float r; // ok ?
+      for (int j=0; j<L; j++){
 
-   //       r = rnd.Rannyu();
-   //       a = (float)i / (float)N;
-   //       b = (float)(i+1) / (float)N;
+         r = rnd.Rannyu();
+         a = (float)i / N;
+         b = (float)(i+1) / N;
 
-   //       if ( (a <= r ) && ( r < b ) ){
-   //          count +=1;
-   //       }
-   //    }
-   //    // cout << count << endl;
-   //    chi2_sum  += ((count - n) * (count - n)) /n;
-   //    chi2_prog[i] = chi2_sum;
-   //    statsfile << count << " " << chi2_sum << " " << chi2_prog[i] << endl;
-   // }
+         if ( (a <= r ) && ( r < b ) ){
+            count +=1;
+         }
+      }
+      chi2_sum  += ((count - n) * (count - n)) /n;
+      chi2_prog[i] = chi2_sum;
+      statsfile << count << " " << chi2_sum << " " << chi2_prog[i] << endl;
+   }
 
-   // statsfile.close();
+   statsfile.close();
 
-   // // 1.2 The Central Limit Theorem
+   // 1.2 The Central Limit Theorem
 
    rnd.SetRandom(seed,p1,p2);
 
-   int n_sums = 10000;
+   int n_sums = 10000; // We sum m random numbers n_sums times
+   vector<int> m_numbers = {1,2,10,100};
 
-   int M1 = 1;
-   int M2 = 2;
-   int M3 = 10;
-   int M4 = 100;
-
-   ofstream statsfile;
+   // Uniform
    statsfile.open("./data/stats_1.2.1.dat");
-
    for (int i = 0; i < n_sums; i++){
-      statsfile << rnd.sum_uniform(M1,0,1) << " ";
-      statsfile << rnd.sum_uniform(M2,0,1) << " ";
-      statsfile << rnd.sum_uniform(M3,0,1) << " ";
-      statsfile << rnd.sum_uniform(M4,0,1);
+      for (int j = 0; j < m_numbers.size(); j++){
+         statsfile << rnd.sum_uniform(m_numbers[j],0,1) << " ";
+      }
       statsfile << endl;
    }
-
    statsfile.close();
 
+   // Exponential
    statsfile.open("./data/stats_1.2.2.dat");
    for (int i = 0; i < n_sums; i++){
-      statsfile << rnd.sum_exponential(M1,1) << " ";
-      statsfile << rnd.sum_exponential(M2,1) << " ";
-      statsfile << rnd.sum_exponential(M3,1) << " ";
-      statsfile << rnd.sum_exponential(M4,1) << endl;
-
+      for (int j = 0; j < m_numbers.size(); j++){
+         statsfile << rnd.sum_exponential(m_numbers[j],1) << " ";
+      }
+      statsfile << endl;
    }
    statsfile.close();
 
+   // Lorentzian
    statsfile.open("./data/stats_1.2.3.dat");
    for (int i = 0; i < n_sums; i++){
-      statsfile << rnd.sum_lorentzian(M1,0,1) << " ";
-      statsfile << rnd.sum_lorentzian(M2,0,1) << " ";
-      statsfile << rnd.sum_lorentzian(M3,0,1) << " ";
-      statsfile << rnd.sum_lorentzian(M4,0,1) << endl;
-
+      for (int j = 0; j < m_numbers.size(); j++){
+         statsfile << rnd.sum_lorentzian(m_numbers[j],0,1) << " ";
+      }
+      statsfile << endl;
    }
    statsfile.close();
 
-   // // 1.3 Buffon Experiment
+   // 1.3 Buffon Experiment
 
-   // rnd.SetRandom(seed,p1,p2);
+   rnd.SetRandom(seed,p1,p2);
 
-   // // Test
+   M = 10000000; // throws
+   N = 100;      // blocks
+   int T = M/N;  // throws per block
+   float d = 10;     // distance between gridlines
+   float length = 5; // needle length
 
-   // int n_throws = 1000000;  // Needles
-   // float d = 10;   // Grid spacing
-   // float length = d/2;   // Needle length
-   // int hits = 0;
+   double t,y1,y2; // random numbers
+   int hits;       // intersections in each block
 
-   // float y1,y2,t;
+   vector<double> pi_estimate(N,0);
+   vector<double> pi2_estimate(N,0);
 
-   // for (int i = 0; i < n_throws; i++){
-   //    t = rnd.RanTheta();
-   //    y1 = d * rnd.Rannyu();
-   //    y2 = y1 + length * sin(t);
-   //    if ((y1<0 || y1>10) || (y2<0 || y2>10)) {
-   //       hits += 1;
-   //    }
-   // }
+   // Run the experiment
+   for (int i = 0; i < N; i++){
+      hits = 0;
+      for (int j = 0; j < T; j++){
+         t = rnd.RanTheta();
+         y1 = d * rnd.Rannyu();
+         y2 = y1 + length * sin(t);
+         if ((y1<0 || y1>10) || (y2<0 || y2>10)) {
+            hits += 1;
+         }
+      }
+      pi_estimate[i] = make_pi(hits, T, d, length);
+      pi2_estimate[i] = pi_estimate[i] * pi_estimate[i];
+   }
 
-   // cout << make_pi(hits,n_throws, d, length) << endl;
-
-   // // Blocked Experiment
-
-   // rnd.SetRandom(seed,p1,p2);
-   // n_throws = 10000000;
-   // // N = 100
-   // int T = n_throws/N;
-
-   // float pi_estimate[N] = {0};
-   // float pi2_estimate[N] = {0};
-
-   // memset(sum_prog, 0, sizeof(sum_prog));
-   // memset(su2_prog, 0, sizeof(su2_prog));
-   // memset(err_prog, 0, sizeof(err_prog));
-
-
-   // statsfile.open("./data/stats_1.3_pi.dat");
-
-   // for (int i = 0; i < N; i++){
-   //    hits = 0;
-   //    for (int j = 0; j < T; j++){
-   //       t = rnd.RanTheta();
-   //       y1 = d * rnd.Rannyu();
-   //       y2 = y1 + length * sin(t);
-   //       if ((y1<0 || y1>10) || (y2<0 || y2>10)) {
-   //          hits += 1;
-   //       }
-   //    }
-
-
-   //    pi_estimate[i] = make_pi(hits, T, d, length);
-   //    pi2_estimate[i] = pi_estimate[i] * pi_estimate[i];
-   //    statsfile << hits << " " << pi_estimate[i] << " " << pi2_estimate[i] << endl;
-   // }
-
-   // statsfile.close();
-
-   // // Statistics
-
-   // statsfile.open("./data/stats_1.3.dat");
-
-   // for (int i=0; i<N; i++){
-   //    for (int j=0; j<(i+1); j++){
-   //       sum_prog[i] += pi_estimate[j]; // cumulative sum of averages
-   //       su2_prog[i] += pi2_estimate[j]; // sum of square averages
-   //    }
-   //    sum_prog[i] /= (i+1); // cumulative average
-   //    su2_prog[i] /= (i+1); // cumulative square average
-   //    err_prog[i] = error(sum_prog, su2_prog, i); // uncertainty
-
-   //    statsfile << sum_prog[i] << " " << su2_prog[i] << " " << err_prog[i] << endl;
-   // }
-
-   // statsfile.close();
+   // Statistics
+   datafile = "./data/stats_1.3.dat";
+   blocked_stats(pi_estimate, pi2_estimate, N, datafile);
 
    rnd.SaveSeed();
    return 0;
-
 }
 
 
