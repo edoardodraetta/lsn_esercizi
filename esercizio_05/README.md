1021024828f56cff1f36e5ad654e1ea0fc1ca549
edoardodraetta <edoardoluigi.draetta@studenti.unimi.it>
2021-07-16 17:06:26 +0200

    Ex 5 final

---
+++ esercizio_05/README.md
@@ -0,0 +1,10 @@
+### Exercise 5
+
+Show a picture of your estimations of the expectation value of `r` in the ground state and in the excited state along with their uncertainties with a large number of throws (M>= 10e6) as a function of the number of blocks, N.
+
+- [x] proof of concept
+- [x] compute <r> in one block
+- [x] compute <r> in multiple blocks
+- [x] excited state
+- [x] gaussian transition probability
+
---
+++ esercizio_05/metropolis.h
@@ -0,0 +1,40 @@
+#include "random.h"
+
+// Positions
+double r0[3];
+double rold[3], r[3];
+
+// Metropolis
+double alpha; // acceptance probability
+double dr; // size of step
+int nsteps, nblocks;
+int accepted, attempted;
+int iprint, imeasure;
+
+// Params
+bool state; // ground or excited state
+bool mode;
+
+// Observables
+double ave_pos, av2_pos;
+double glob_ave, glob_av2, err;
+double blk_norm;
+
+// RNG
+Random rnd;
+int seed[4];
+int p1, p2;
+
+void Initialize();
+void Welcome();
+void Move();
+void Accumulate(int);
+void Average(int);
+void Reset(int);
+void Report(int);
+void PrintPos(double []);
+
+double GroundState(double []);
+double ExcitedState(double []);
+double DistanceFormula(double []);
+double Error(double, double, int);
